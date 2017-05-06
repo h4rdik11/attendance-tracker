6a45bd55d6ccd2b52d01b9f766eb39caf0b1506e
Hardik Chauhan <Hardik Chauhan>
2017-05-06 15:44:34 +0530

    Minor changes

--- public/js/controllers/userCtrl.js
+++ public/js/controllers/userCtrl.js
@@ -1,14 +1,7 @@
 app.controller('UserController', function($scope, $http, $auth, $location){
 
     if(! $auth.isAuthenticated()) $location.url('/');
-    $scope.user = {};
-    $scope.addUser = function(){
-      // alert($scope.user.name);
-      $http.post("http://localhost:3030/api/add-user", $scope.user).then(function(response){
-        alert(response.data);
-      });
-    };
-
+    
     $scope.loggedUser = {};
     $scope.getUser = function(){
       $http.get("http://localhost:3030/api/get-user?token="+$auth.getToken()).then(function(response){
