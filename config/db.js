1a3f4fe1c5b5f6c9e354a9c9a9638cde350a36e1
Hardik Chauhan <Hardik Chauhan>
2017-05-06 21:57:10 +0530

    For heroku

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 //configuring database
 
 module.exports = {
-  url : "mongodb://localhost:27017/attendance_db", 
+  url : "mongodb://root:H4rdik1781@ds127101.mlab.com:27101/attendance_db", 
   secret: "H4rdik@1781"
 };
